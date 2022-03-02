f1a07709ba80a29375302cc4c77bfb799033b4ab
mmrosatab <mmrosatab@hotmail.com>
2022-03-02 19:17:55 -0300

    Style: Adding responsiveness in background app

--- nozbooks-app/src/pages/Login/styles.js
+++ nozbooks-app/src/pages/Login/styles.js
@@ -2,14 +2,11 @@ import styled from "styled-components";
 import bgi from "../../assets/images/background_login.svg";
 
 const Container = styled.div`
-  background-image: url("${bgi}");
+  width: 100vw;
+  height: 100vh;
+  background: url("${bgi}") center center;
   background-size: cover;
-  background-repeat: no-repeat;
-  height: 100%;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
 `;
 
 export default Container;
